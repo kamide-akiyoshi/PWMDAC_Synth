1c3822927f3066d59bf941d87e174e667e822ab2
Akiyoshi Kamide <kamide@yk.rim.or.jp>
2016-04-21 00:36:05 +0900

    スケッチの例を最新バージョンに合わせて更新

--- examples/MIDI/MIDI.ino
+++ examples/MIDI/MIDI.ino
@@ -6,11 +6,15 @@
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 #define MIDI_ENABLE_PIN     2
-
 #define PWMDAC_OUTPUT_PIN   3
 #include <PWMDAC_Synth.h>
-const EnvelopeParam DEFAULT_ENV_PARAM = EnvelopeParam(4, 10, 128, 8);
-PWMDAC_CREATE_INSTANCE(sineWavetable, PWMDAC_SINE_WAVE, DEFAULT_ENV_PARAM);
+
+PWMDAC_CREATE_WAVETABLE(sineWavetable, PWMDAC_SINE_WAVE);
+
+// {wavetableArray, {release, sustain, decay, attack}}
+PROGMEM const Instrument instrument = {sineWavetable, {9, 128, 8, 3}};
+
+PWMDAC_CREATE_INSTANCE(&instrument);
 
 void HandleNoteOn(byte channel, byte pitch, byte velocity) { 
   if( velocity == 0 ) {
